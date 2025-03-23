1121e2b2d42b71900360dfbf37a83b2e9ed20379
Leela07 <162034645+leelajk@users.noreply.github.com>
2025-03-23 09:28:08 +0530

    main.py

--- appengine/standard_python3/hello_world/main.py
+++ appengine/standard_python3/hello_world/main.py
@@ -29,7 +29,7 @@ def hello():
     Returns:
         A string with the words 'Hello World!'.
     """
-    return "Hello World!"
+    return "Hello Leela! Welcome to cloud Computing!!"
 
 
 if __name__ == "__main__":
